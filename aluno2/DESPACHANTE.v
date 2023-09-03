b193a8564a91cb230e1bba17d80f389747a9e308
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-09-03 13:21:12 -0300

    adicionando o DESPACHANTE

---
+++ aluno2/DESPACHANTE.v
@@ -0,0 +1,23 @@
+/** Este módulo indica qual sensor irá receber os dados presentes no buffer do receptor do uart 
+A idéia é que quando o uart falar que terminou de receber algo, o despachante olhe o endereço e assim diga 
+ao módulo de sensoriamento que pode começar a ler os dados que estão no barramento. (a saída do uart rx vai
+para a entrada de todos os sensores) aí ele olha oq o comando pede e faz oq precisa ser feito
+
+- Cada 'pin_to_module' vai em um módulo de sensoriamento*/
+
+module DESPACHANTE(input uart_rx_ready, // Pino do Uart RX que indica quando terminou o processo de recebimento de dados
+						input [4:0] addres_req,// Parte dos dados recebidos pela uart que indica o endereço 
+						output [31:0] pin_to_module);// Um pino para cada módulo dos 32 sensores
+	//
+	assign pin_to_module[0] = (addres_req == 5'b00000) & uart_rx_ready;
+	assign pin_to_module[1] = (addres_req == 5'b00001) & uart_rx_ready;
+	assign pin_to_module[2] = (addres_req == 5'b00010) & uart_rx_ready;
+	assign pin_to_module[3] = (addres_req == 5'b00011) & uart_rx_ready;
+	assign pin_to_module[4] = (addres_req == 5'b00100) & uart_rx_ready;
+	assign pin_to_module[5] = (addres_req == 5'b00101) & uart_rx_ready;
+	assign pin_to_module[6] = (addres_req == 5'b00110) & uart_rx_ready;
+	assign pin_to_module[7] = (addres_req == 5'b00111) & uart_rx_ready;
+	assign pin_to_module[8] = (addres_req == 5'b01000) & uart_rx_ready;
+	//
+	
+endmodule
--- aluno2/UART.v
+++ aluno2/UART.v
@@ -5,7 +5,7 @@ module UART (
 	input [15:0] data_to_send, // Dados que serão enviados
 	output [15:0] data_recevied, // Dados recebidos pela UART
 	output tx,// Pino para transmitir os dados
-	output data_ready_to_read,// Informa que no buffer do receptor há dados prontos para serem usado (o processo de recebimento acabou)
+	output data_ready_to_read// Informa que no buffer do receptor há dados prontos para serem usado (o processo de recebimento acabou)
 	);
 
 	UART_tx receptor(clk_9k6, rx, data_recevied, data_ready_to_read);

4fa05d791c7f36dc05a1a7c5a2ffa9c749ec7860
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-09-04 20:30:10 -0300

    adicionando saída para avisar que transmissão acabou

--- aluno2/UART.v
+++ aluno2/UART.v
@@ -5,10 +5,11 @@ module UART (
 	input [15:0] data_to_send, // Dados que serão enviados
 	output [15:0] data_recevied, // Dados recebidos pela UART
 	output tx,// Pino para transmitir os dados
-	output data_ready_to_read// Informa que no buffer do receptor há dados prontos para serem usado (o processo de recebimento acabou)
+	output data_ready_to_read,// Informa que no buffer do receptor há dados prontos para serem usado (o processo de recebimento acabou)
+	output finish_transmission// Informa que a transmissão acabou
 	);
 
 	UART_tx receptor(clk_9k6, rx, data_recevied, data_ready_to_read);
-	UART_tx transmissor(clk_9k6, tx, data_to_send, enable_transmiter);
+	UART_tx transmissor(clk_9k6, tx, data_to_send, enable_transmiter, finish_transmission);
 
 endmodule

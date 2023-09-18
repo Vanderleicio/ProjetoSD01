9e2d8aaa67ec74ebb8112ed1c22111d26e50f9d1
Wagner Alexandre <68972860+WagnerAlexandre@users.noreply.github.com>
2023-09-18 13:23:41 -0300

    :wrench: fix: Ordem de requisição

--- aluno2/codigosEmC/garcom.c
+++ aluno2/codigosEmC/garcom.c
@@ -43,7 +43,7 @@ int main(){
         scanf("%d",&comando);
 
         // Simulação de escrita na porta (substitua por implementação)
-        envio[0] = srsAddress; envio[1] =comando; envio[2] = '\0';
+        envio[0] = comando ; envio[1] =srsAddress; envio[2] = '\0';
         printf("\n\nEndereco do sensor: %s\n", envio);
 
         printf("Digite o endereco de um sensor ou -1 para sair\nAguardando: ");

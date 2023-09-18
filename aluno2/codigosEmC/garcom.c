5852f22e96b7544a54a56bb901b689c164cd8914
Wagner Alexandre <68972860+WagnerAlexandre@users.noreply.github.com>
2023-09-17 22:43:22 -0300

    :sparkles: Garçom 1.0
    Testar implementação

--- aluno2/codigosEmC/garcom.c
+++ aluno2/codigosEmC/garcom.c
@@ -7,6 +7,7 @@
 
 int main(){
 	int fd, len, resp;
+	unsigned char envio[2]; // vetor que e enviado
 	struct termios options; /* Serial ports setting */
 	// Informando a porta, que é de leitura e escrita, sem delay
 	fd = open("/dev/ttyS0", O_WRONLY | O_NDELAY | O_NOCTTY);
@@ -38,20 +39,15 @@ int main(){
     scanf("%d", &srsAddress);
 
     while (srsAddress != -1) {
-        while (getchar() != '\n'); // Limpar o buffer de entrada
-
         printf("Digite um comando: ");
-        scanf("%2s\n",&comando);
+        scanf("%d",&comando);
 
-
-        // Simulação de escrita na porta (substitua por sua implementação real)
-        printf("\n\nEndereco do sensor: %d\n", srsAddress);
-        printf("Comando:  0x%02hhX\n", comando);
-
-        // Agora você pode escrever os dados na porta real, substitua esta parte pelo seu código real
+        // Simulação de escrita na porta (substitua por implementação)
+        envio[0] = srsAddress; envio[1] =comando; envio[2] = '\0';
+        printf("\n\nEndereco do sensor: %s\n", envio);
 
         printf("Digite o endereco de um sensor ou -1 para sair\nAguardando: ");
-        scanf("%d\n", &srsAddress);
+        scanf("%d", &srsAddress);
     }
 
     close(fd);
